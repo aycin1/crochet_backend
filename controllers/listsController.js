d8b6c67b87bccc67222ebbfbd7322f5cac69bddc
aycin <aycinkaya98@gmail.com>
2025-12-02 12:38:15 +0000

    db query changes

--- controllers/listsController.js
+++ controllers/listsController.js
@@ -8,11 +8,11 @@ async function getLists(req, res) {
 
   try {
     listsQuery = await db.query(
-      "SELECT l.name FROM lists l JOIN users u ON l.user_id=u.user_id WHERE u.username = $1;",
+      "SELECT name FROM lists WHERE user_id = (SELECT user_id FROM users WHERE username = $1);",
       [username]
     );
     patternQuery = await db.query(
-      "SELECT p.pattern_id, l.name FROM patterns p JOIN lists l ON p.list_id = l.list_id JOIN users u ON p.user_id = u.user_id WHERE u.username=$1;",
+      "SELECT p.pattern_id, l.name FROM patterns p JOIN lists l ON p.list_id = l.list_id WHERE l.user_id = (SELECT user_id FROM users WHERE username = $1);",
       [username]
     );
   } catch (error) {
@@ -35,7 +35,7 @@ async function handlePatternAddition(req, res) {
 
   try {
     addPattern = await db.query(
-      "INSERT INTO patterns (pattern_id, user_id, list_id) SELECT $1, u.user_id, l.list_id FROM lists l LEFT JOIN users u ON u.user_id = l.user_id WHERE l.name = $2 AND u.username = $3;",
+      "INSERT INTO patterns (pattern_id, user_id, list_id) SELECT $1, l.user_id, l.list_id FROM lists l JOIN users u ON l.user_id = u.user_id WHERE l.list_id = (SELECT list_id FROM lists l WHERE l.name = $2 AND l.user_id = (SELECT user_id FROM users WHERE username = $3));",
       [pattern_id, list, username]
     );
   } catch (error) {
